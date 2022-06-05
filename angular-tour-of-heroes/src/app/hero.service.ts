0c20fce81903f9f0c2db02245729713340575fb7
Renan Ribeiro <renan@itexto.com.br>
2022-06-05 12:38:22 -0300

    primeiro commit

---
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Hero } from './hero';
+import { MessageService } from './message.service';
+import { HEROES } from './mock-heroes';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    this.messageService.add('Adicionei os herois')
+    return heroes;
+  }
+
+  constructor(private messageService: MessageService) { }
+}
